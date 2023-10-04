5a4470a9032b70d4d1229c3bd2374aa9f47d0aa9
Juan Real <104905324+therealjuan@users.noreply.github.com>
2023-10-04 09:59:35 +0200

    Updated divs to make stickers move

--- app/components/onutSticker.tsx
+++ app/components/onutSticker.tsx
@@ -6,8 +6,12 @@ import ONUTLogo from '/public/onut-sticker.svg'
 export const ONUTSticker = () => {
 
         return (
-            <div className="absolute top-32 right-12 right-0 z-1 bg-white rounded-full w-36 border-white border-10 transition transition-transform duration-400 hover:rotate-20 hidden lg:block draggable">
-                <Image src={ONUTLogo} alt="ONUT" className="w-full" />
+            <div>
+                <div className='draggable'>
+                    <div className="absolute top-32 right-12 right-0 z-1 bg-white rounded-full w-36 border-white border-10 transition transition-transform duration-400 hover:rotate-20 hidden lg:block">
+                        <Image src={ONUTLogo} alt="ONUT" className="w-full" />
+                    </div>
+                </div>
             </div>
     )
 };
--- app/components/toolSticker.tsx
+++ app/components/toolSticker.tsx
@@ -7,9 +7,13 @@ export const ToolSticker = () => {
     const t = useTranslations("Index.toolSticker");
 
     return (
-        <div className="absolute top-36 right-[28rem] z-1 rotate-[6deg] max-lg:hidden draggable">
-            <div className='text-6xl text-purple-900 font-black' id="stickerise" data-sticker-text="Palette.fm">Palette.fm</div>
-            <span className="bg-white px-2 ml-[-4.2px]">{t("lastToolAdded")}</span>
+        <div>
+            <div className='draggable'>
+                <div className="absolute top-36 right-[28rem] z-1 rotate-[6deg] max-lg:hidden">
+                    <div className='text-6xl text-purple-900 font-black' id="stickerise" data-sticker-text="Palette.fm">Palette.fm</div>
+                    <span className="bg-white px-2 ml-[-4.2px]">{t("lastToolAdded")}</span>
+                </div>
+            </div>
         </div>
     )
 };
